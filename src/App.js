6c9ef39c95138e43af941e167a79714e178c86ad
dymafr <erwan1309@hotmail.com>
2022-07-02 09:45:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let ignore = false;
+    async function fetchTodoList() {
+      try {
+        const reponse = await fetch('https://restapi.fr/api/todo');
+        if (reponse.ok) {
+          const todos = await reponse.json();
+          if (!ignore) {
+            if (Array.isArray(todos)) {
+              setTodoList(todos);
+            } else {
+              setTodoList([todos]);
+            }
+          }
+        } else {
+          console.error('Oops, une erreur');
+        }
+      } catch (e) {
+      } finally {
+        if (!ignore) {
+          setLoading(false);
+        }
+      }
+    }
+    fetchTodoList();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  function addTodo(todo) {
+    setTodoList([...todoList, todo]);
+  }
+
+  function deleteTodo(id) {
+    setTodoList(todoList.filter((todo) => todo._id !== id));
+  }
+
+  function toggleTodo(id) {
+    setTodoList(
+      todoList.map((todo) =>
+        todo._id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+
+  function toggleTodoEdit(id) {
+    setTodoList(
+      todoList.map((todo) =>
+        todo._id === id ? { ...todo, edit: !todo.edit } : todo
+      )
+    );
+  }
+
+  function editTodo(id, content) {
+    setTodoList(
+      todoList.map((todo) =>
+        todo._id === id ? { ...todo, edit: false, content } : todo
+      )
+    );
+  }
+
+  function selectTodo(id) {
+    setTodoList(
+      todoList.map((todo) =>
+        todo._id === id
+          ? { ...todo, selected: !todo.selected }
+          : { ...todo, selected: false }
+      )
+    );
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center p-20">
+      <div className="card container p-20">
+        <h1 className="mb-20">Liste de tâches</h1>
+        <AddTodo addTodo={addTodo} />
+        {loading ? (
+          <p>Chargement en cours...</p>
+        ) : (
+          <TodoList
+            todoList={todoList}
+            deleteTodo={deleteTodo}
+            toggleTodo={toggleTodo}
+            toggleTodoEdit={toggleTodoEdit}
+            editTodo={editTodo}
+            selectTodo={selectTodo}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import EditTodo from './EditTodo';
+
+export default function TodoList({
+  todoList,
+  deleteTodo,
+  toggleTodo,
+  toggleTodoEdit,
+  editTodo,
+  selectTodo,
+}) {
+  return todoList.length ? (
+    <ul>
+      {todoList.map((todo) =>
+        todo.edit ? (
+          <EditTodo
+            key={todo._id}
+            todo={todo}
+            editTodo={(content) => editTodo(todo._id, content)}
+            cancelEditTodo={() => toggleTodoEdit(todo._id)}
+          />
+        ) : (
+          <TodoItem
+            key={todo._id}
+            todo={todo}
+            deleteTodo={() => deleteTodo(todo._id)}
+            toggleTodo={() => toggleTodo(todo._id)}
+            editTodo={() => toggleTodoEdit(todo._id)}
+            selectTodo={() => selectTodo(todo._id)}
+          />
+        )
+      )}
+    </ul>
+  ) : (
+    <p>Aucune tâche en cours </p>
+  );
+}
